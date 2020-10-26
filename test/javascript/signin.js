55f2434f340c513f0123e6963ecfe7404cd68a84
Skanda2561 <skanda.s2001@gmail.com>
2020-10-26 19:55:36 +0530

    this commit has mjor changes : 1) changed color scheme and added dark mode 2) added signin and signup functionality 3) completed host and student page basic structure 4) some minor style changes

--- test/javascript/signin.js
+++ test/javascript/signin.js
@@ -1,17 +1,124 @@
-
+//transitions of signin and signup
 function toggleForm() {
-  var container=document.querySelector('.login .container');
+  var container=document.querySelector('.login .container')
   container.classList.toggle('active')
 }
-function userSignup(){
-  document.querySelector('.login ').style.display="none";;
-  document.querySelector('.signup-prompt').style.display="none";
-  document.querySelector('.signin-prompt').style.display="flex";
-  document.querySelector('.signup .user .formbox form').style.display="block";
+
+
+//user details
+var studentobj = [
+	{
+		username: "stud",
+		password: "stud"
+	}
+]
+
+var hostobj = [
+	{
+		username: "host",
+		password: "host"
+	}
+]
+
+// login functionality for students
+function studlogin()
+{	// retreive data from username and store in username variable
+	var username = document.getElementById("studname").value
+	// retreive data from password and store in password variable
+	var password = document.getElementById("spword").value
+  var done =-1
+	// loop through all the user pbjects and confrim if the username and password are correct
+	for(var i = 0; i < studentobj.length; i++)
+  {
+		if(username ==studentobj[i].username && password == studentobj[i].password) {
+      location.replace("student.html")
+      localStorage.setItem("username", username)
+      done=1
+			// stop the statement if result is found true
+			return
+		}
+	}
+  // error if username and password don't match
+  if(done==-1)
+    alert('incorrect username or password')
 }
-function userSignin(){
-  document.querySelector('.login ').style.display="flex";;
-  document.querySelector('.signup-prompt').style.display="flex";
-  document.querySelector('.signin-prompt').style.display="none";
-  document.querySelector('.signup .user .formbox form').style.display="none";
+
+
+// login functionality for hosts
+function hostlogin()
+{
+	// retreive data from username and store in username variable
+	var username = document.getElementById('hostname').value
+	// retreive data from password and store in password variable
+	var password = document.getElementById('hpword').value
+  var done =-1
+	// loop through all the user pbjects and confrim if the username and password are correct
+	for(var i = 0; i < hostobj.length; i++)
+  {	// check to
+		if(username == hostobj[i].username && password == hostobj[i].password)
+    {
+      location.replace("host.html")
+      localStorage.setItem("username", username)
+      done=1
+			// stop the statement if result is found true
+			return
+		}
+	}
+    // error if username and password don't match
+  if(done==-1)
+    alert('incorrect username or password')
+}
+
+
+// register functionality
+function registerUser() {
+	// store new users username
+	var Username = document.getElementById('uname').value
+  // store new user type
+  var Usertype = document.getElementById('usertype').value
+	// store new users password
+	var Password = document.getElementById('crpword').value
+  var Passwordc = document.getElementById('copword').value
+  //test whether both passwords are same
+  if(Password!=Passwordc)
+    alert("Passwords do not match. Please check and try again!!! ")
+	// store new user data in an object
+	var newUser = {
+		username: Username,
+		password: Password
+	}
+  if(Usertype=='student')
+  {
+    // loop throught people array to see if the username is taken, or password to short
+  	for(var i = 0; i < studentobj.length; i++)
+    {
+  		if(User == studentobj[i].username)
+      {
+  			alert('That username is already in use, please choose another')
+  			return
+  		}
+		}
+
+  	// push new object to the people array
+  	studentobj.push(newUser)
+    alert("Thank you for trusting us. Your responses are recorded, we will contact you for confirmation and reflect the changes.")
+    return
+}
+  else if(Usertype=='host')
+  {
+    // loop throught people array to see if the username is taken, or password to short
+  	for(var i = 0; i < hostobj.length; i++)
+    {
+  		if(Username == hostobj[i].username)
+      {
+  			alert('That username is alreay in user, please choose another')
+  			return
+  		}
+  	}
+  	// push new object to the people array
+  	hostobj.push(newUser)
+      alert("Thank you for trusting us. Your responses are recorded, we will contact you for confirmation and reflect the changes.")
+      return
+  }
+
 }
